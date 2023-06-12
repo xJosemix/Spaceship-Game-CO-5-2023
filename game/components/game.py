375fc7182275cc211562384db54439ed1804b063
Karem Huacota <karem.huacota@gmail.com>
2023-06-12 17:24:53 -0400

    Initial commit

---
+++ game/components/game.py
@@ -0,0 +1,51 @@
+import pygame
+
+from game.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS, DEFAULT_TYPE
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption(TITLE)
+        pygame.display.set_icon(ICON)
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.playing = False
+        self.game_speed = 10
+        self.x_pos_bg = 0
+        self.y_pos_bg = 0
+
+    def run(self):
+        # Game loop: events - update - draw
+        self.playing = True
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+        pygame.display.quit()
+        pygame.quit()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.playing = False
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.clock.tick(FPS)
+        self.screen.fill((255, 255, 255))
+        self.draw_background()
+        pygame.display.update()
+        pygame.display.flip()
+
+    def draw_background(self):
+        image = pygame.transform.scale(BG, (SCREEN_WIDTH, SCREEN_HEIGHT))
+        image_height = image.get_height()
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg))
+        self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
+        if self.y_pos_bg >= SCREEN_HEIGHT:
+            self.screen.blit(image, (self.x_pos_bg, self.y_pos_bg - image_height))
+            self.y_pos_bg = 0
+        self.y_pos_bg += self.game_speed
---
+++ game/utils/constants.py
@@ -0,0 +1,30 @@
+import pygame
+import os
+
+# Global Constants
+TITLE = "Spaceships Game"
+SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1100
+FPS = 30
+IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
+
+# Assets Constants
+ICON = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
+
+SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
+
+BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png'))
+
+HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png'))
+
+DEFAULT_TYPE = "default"
+SHIELD_TYPE = 'shield'
+
+SPACESHIP = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
+SPACESHIP_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship_shield.png"))
+BULLET = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_1.png"))
+
+BULLET_ENEMY = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_2.png"))
+ENEMY_1 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_1.png"))
+
+FONT_STYLE = 'freesansbold.ttf'
